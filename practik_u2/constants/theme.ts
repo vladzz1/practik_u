61aa62c0bb25f50be0996fd36c7ad7ca98e55018
vladzz1 <vvladz1106@gmail.com>
2026-09-14 17:22:24 +0300

    add nativewind

--- practik_u2/constants/theme.ts
+++ practik_u2/constants/theme.ts
@@ -3,6 +3,8 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
+import "@/global.css"
+
 import { Platform } from 'react-native';
 
 const tintColorLight = '#0a7ea4';
